9a7519f0b0521a9525606c34fb6a113416126048
solrac031504 <146136069+solrac031504@users.noreply.github.com>
2023-11-19 20:44:18 -0500

    Update literal_table.c
    Added linked list manipulation and implemented size detection functions

--- literal_table.c
+++ literal_table.c
@@ -1,17 +1,92 @@
+#include <stdlib.h>
+#include <string.h>
+
 #include "literal_table.h"
+#include "utilities.h"
+
+typedef struct Node
+{
+	char *data;
+	struct Node *next;
+} Node;
+
+typedef struct List
+{
+	Node *head;
+	Node *tail;
+	unsigned int size;
+} List;
+
+// literal table is in the form of a linked list
+List *table;
+
+// functions for linked list manipulation
+Node *table_create_node(Node *curr, char *data);
+void table_add(char *data);
+void table_remove(char *key);
+bool table_search(char *key);
+void table_destroy(Node *head);
+
+Node *table_create_node(Node *curr, char *data)
+{
+	curr = malloc(sizeof(Node));
+	curr->data = malloc(sizeof(char) * (sizeof(data) + 1));
+	strcpy(curr->data, data);
+	curr->next = NULL;
+
+	return curr;
+}
+
+void table_add(char *data)
+{
+	if (table->head == NULL)
+	{
+		table->head = table_create_node(table->head, data);
+		table->tail = table->head;
+	}
+	else
+	{
+		table->tail->next = table_create_node(table->tail->next, data);
+		table->tail = table->tail->next;
+	}
+
+	table->size++;
+}
+
+bool table_search(char *key)
+{
+	Node *curr = table->head;
+	while (curr != NULL)
+	{
+		if (!strcmp(curr->data, key))
+			return true;
+	}
+
+	return false;
+}
+
+void table_destroy(Node *head)
+{
+	if (head == NULL) return;
+
+	table_destroy(head->next);
+
+	free(head->data);
+	free(head);
+}
+
+// ========== BEGINNING OF LITERAL TABLE FUNCTIONS ==========
 
 // Return the size (in words/entries) in the literal table
 unsigned int literal_table_size()
 {
-	bail_with_error("TODO: no implementation of literal_table_size yet!");
-	return 0;
+	return table->size;
 }
 
 // is the literal_table empty?
 bool literal_table_empty()
 {
-	bail_with_error("TODO: no implementation of literal_table_empty yet!");
-	return false;
+	return (table->size == 0) ? true : false;
 }
 
 // is the literal_table full?
@@ -24,7 +99,7 @@ bool literal_table_full()
 // initialize the literal_table
 void literal_table_initialize()
 {
-	bail_with_error("TODO: no implementation of literal_table_initialize yet!");
+	table = calloc(1, sizeof(List));
 }
 
 // Return the offset of sought if it is in the table,
